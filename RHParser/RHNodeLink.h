96d2ab259a5c499e5e2129d27faecde0adf4ec1a
zhu410289616 <zhu410289616@163.com>
2017-05-08 22:46:10 +0800

    处理link的数据 为后续点击时间做准备

---
+++ RHParser/RHNodeLink.h
@@ -0,0 +1,13 @@
+//
+//  RHNodeLink.h
+//  Example
+//
+//  Created by zhuruhong on 2017/5/3.
+//  Copyright © 2017年 zhuruhong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RHNodeLink : NSObject
+
+@end
---
+++ RHParser/RHNodeLink.m
@@ -0,0 +1,13 @@
+//
+//  RHNodeLink.m
+//  Example
+//
+//  Created by zhuruhong on 2017/5/3.
+//  Copyright © 2017年 zhuruhong. All rights reserved.
+//
+
+#import "RHNodeLink.h"
+
+@implementation RHNodeLink
+
+@end
