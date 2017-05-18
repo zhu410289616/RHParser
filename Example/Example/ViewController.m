19f6971e14e2c5c7837d9df32601981b000280fa
zhu410289616 <zhu410289616@163.com>
2017-05-18 23:00:07 +0800

    测试数据 准备写readme

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -12,6 +12,8 @@
 #import "RHHtmlParser.h"
 #import "RHXmlParser.h"
 
+#import "RHNode.h"
+
 #import "ShowRichTextController.h"
 
 @interface ViewController ()
@@ -30,6 +32,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
     
     _testButton = [UIButton buttonWithType:UIButtonTypeCustom];
     _testButton.frame = CGRectMake(20, 80, 100, 45);
@@ -52,9 +56,31 @@
     [_xmlButton addTarget:self action:@selector(doXmlButtonAction) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:_xmlButton];
     
-    NSString *htmlString = @"<font color='#00ff00' bgcolor='#fa8919'>bai<font size='24' color='#ff00ff'>This <strike>is aaa</strike> some text!</font>bai</font><font color='#000000'>价格：</font><font color='#00ffff'>33.4</font><font color='#00ff00'>bai<font size='24' color='#ff00ff'>This <strike>is aaa</strike> some text!</font>bai</font><font color='#000000'>价格：</font><font color='#00ffff'>33.4</font><img src='www.baidu.com'><a href='https://www.baidu.com/img/bd_logo1.png'><img src='https://www.baidu.com/img/bd_logo1.png'></a><p>pppp</p><p>哈哈哈<font color=\"#fa8919\" size=\"58\">43333333</font>我是置顶消息摘要－消息<font color=\"#345643\" size=\"34\">（二）消息</font><font size=\"12\">dfs太容易让他的身份发生地方</font>卡上开发的乐山大佛</p><img src='item_67_s_122010.png' width='60' height='65'> <p>title<span>111</span><a href=\"https://www.baidu.com\">222</a>333<br><i>77&nbsp;7<strong>555</strong>444</i>666</p><strike> strike </strike> <stroke>stroke</stroke> <img src=\"item_72_s_122010.png\" width='60' height='65'><img src=\"item_72_s_122010.png\" width='60' height='65'>岁地方";
+    NSString *htmlString = @"<username>我是username自定义样式</username><password size='20'>我是password自定义样式，注意属性内容</password><font color='#00ff00' bgcolor='#fa8919'>bai<font size='24' color='#ff00ff'>This <strike>is aaa</strike> some text!</font>bai</font><font color='#000000'>价格：</font><font color='#00ffff'>33.4</font><font color='#00ff00'>bai<font size='24' color='#ff00ff'>This <strike>is aaa</strike> some text!</font>bai</font><font color='#000000'>价格：</font><font color='#00ffff'>33.4</font><img src='www.baidu.com'><a href='https://www.baidu.com/img/bd_logo1.png'><img src='https://www.baidu.com/img/bd_logo1.png'></a><p>pppp</p><p>哈哈哈<font color=\"#fa8919\" size=\"58\">43333333</font>我是置顶消息摘要－消息<font color=\"#345643\" size=\"34\">（二）消息</font><font size=\"12\">dfs太容易让他的身份发生地方</font>卡上开发的乐山大佛</p><img src='item_67_s_122010.png' width='60' height='65'> <p>title<span>111</span><a href=\"https://www.baidu.com\">222</a>333<br><i>77&nbsp;7<strong>555</strong>444</i>666</p><strike> strike </strike> <stroke>stroke</stroke> <img src=\"item_72_s_122010.png\" width='60' height='65'><img src=\"item_72_s_122010.png\" width='60' height='65'>岁地方";
     
     RHHtmlParser *htmlParser = [[RHHtmlParser alloc] init];
+    
+    //自定义样式
+    NSDictionary *customStyles = @{
+                                   NSForegroundColorAttributeName: [UIColor orangeColor],
+                                   NSBackgroundColorAttributeName: [UIColor blueColor]
+                                   };
+    [htmlParser addStyle:customStyles forTag:@"username"];
+    
+    [htmlParser addNodeBlock:^NSAttributedString *(RHNode *node, NSDictionary *defaultStyles) {
+        NSString *theContent = node.content ?: @"";
+        
+        NSString *theSizeString = node.attributes[@"size"];
+        NSDictionary *theStyle = @{
+                                   NSForegroundColorAttributeName: [UIColor redColor],
+                                   NSFontAttributeName: [UIFont systemFontOfSize:[theSizeString floatValue]]
+                                   };
+        
+        NSAttributedString *theAttributedString = [[NSAttributedString alloc] initWithString:theContent attributes:theStyle];
+        
+        return theAttributedString;
+    } forTag:@"password"];
+    
     NSAttributedString *attributedString = [htmlParser parseString:htmlString filter:nil];
     
     _htmlLabel = [[UILabel alloc] init];
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # RHParser
 html、xml等标记语言解析。可以方便的转换为NSAttributedString。
+
+### 把html转换为NSAttributedString
+	NSString *htmlString = @"<font color=\"#fa8919\" size='58'>43333333</font>";
+	RHHtmlParser *htmlParser = [[RHHtmlParser alloc] init];
+	NSAttributedString *attributedString = [htmlParser parseString:htmlString filter:nil];
+
+### 自定义样式
+	NSString *htmlString = @"<username>我是username自定义样式</username><password size='20'>我是password自定义样式，注意属性内容</password>";
+	//自定义样式
+	NSDictionary *customStyles = @{
+                                   NSForegroundColorAttributeName: [UIColor orangeColor],
+                                   NSBackgroundColorAttributeName: [UIColor blueColor]
+                                   };
+	[htmlParser addStyle:customStyles forTag:@"username"];
+	[htmlParser addNodeBlock:^NSAttributedString *(RHNode *node, NSDictionary *defaultStyles) {
+        NSString *theContent = node.content ?: @"";
+        
+        NSString *theSizeString = node.attributes[@"size"];
+        NSDictionary *theStyle = @{
+                                   NSForegroundColorAttributeName: [UIColor redColor],
+                                   NSFontAttributeName: [UIFont systemFontOfSize:[theSizeString floatValue]]
+                                   };
+        
+        NSAttributedString *theAttributedString = [[NSAttributedString alloc] initWithString:theContent attributes:theStyle];
+        
+        return theAttributedString;
+    } forTag:@"password"];
+	NSAttributedString *attributedString = [htmlParser parseString:htmlString filter:nil];
+
+
+
